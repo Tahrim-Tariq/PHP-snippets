6d957241cf4921b5eb08ff9b68a0ad02e4217a05
Tahrim-Tariq <101403730+Tahrim-Tariq@users.noreply.github.com>
2022-06-02 19:40:33 +0500

    PHP TABLE 2

---
+++ tableWithFunc.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+      body{
+         text-align:center;
+      }
+      table{
+         margin:auto;
+         padding:auto;
+      }
+   </style>
+</head>
+<body>
+   
+   <?php
+   echo "<table border=1 >
+   <th>Name</th>
+   <th>Roll no.</th>
+   <th>Number</th>
+   <th>Age</th>
+   <tr>
+   <td>Jannat</td>
+   <td>two</td>
+   <td>032092748342</td>
+   <td>20</td>
+   </tr>
+   <tr>
+   <td>Tehreem</td>
+   <td>two</td>
+   <td>032092748342</td>
+   <td>21</td>
+   </tr>
+   <tr>
+   <td>Gull</td>
+   <td>two</td>
+   <td>032092748342</td>
+   <td>23</td>
+   </tr>
+   </table>";
+   ?>
+</body>
+</html>
