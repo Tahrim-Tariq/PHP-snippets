031db852724c673b4cdea75cc1ed337f3a462646
Tahrim-Tariq <101403730+Tahrim-Tariq@users.noreply.github.com>
2022-06-02 19:36:07 +0500

    CHESS-BORAD

---
+++ chess-board.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #table1{
+            
+            border-collapse: collapse;
+            border: 2px solid black;
+        }
+    </style>
+</head>
+<body> 
+  <h3>Chess Board using Nested For Loop</h3>
+   <table id='table1' width="270px" cellspacing="0px" cellpadding="0px">
+   <!-- cell 270px wide (8 columns x 60px) -->
+      <?php
+      for($row=1;$row<=8;$row++)
+	  {
+          echo "<tr>";
+          for($col=1;$col<=8;$col++)
+		  {
+          $total=$row+$col;
+          if($total%2==0)
+		  {
+          echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+          }
+		  else
+		  {
+          echo "<td height=30px width=30px bgcolor=#000000></td>";
+          }
+          }
+          echo "</tr>";
+    }
+          ?>
+</body>
+</html>
